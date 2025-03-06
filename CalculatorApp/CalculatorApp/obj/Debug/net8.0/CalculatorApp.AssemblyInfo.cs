bfbdb93e09a4107167f0cb79231ecd49d5f6f308
Ganesh9170 <ganesh.kende@zensar.com>
2025-03-06 12:23:29 +0530

    editing the code in the Calculator application

--- CalculatorApp/CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
+++ CalculatorApp/CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5d4e31ddc6a68932e0ffed9b28340750a4f938")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
